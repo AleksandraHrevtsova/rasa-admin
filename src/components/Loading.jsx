58cdf688e63cee95e10d9936f694568a7a2cbfe6
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-03-31 03:32:18 +0300

    main and common components

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import { useLocale } from "../contexts/LocaleContext";
+
+export const Loading = () => {
+  const { t } = useLocale();
+  return (
+    <div className='p-4'>{t['loading']}</div>
+  )
+};

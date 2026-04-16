a00b139dc24655901fb3bcf0e906c8a821c18ec3
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-04-17 01:55:19 +0300

    splited localizations

---
+++ src/core/i18n/keys.js
@@ -0,0 +1,100 @@
+export const keys = {
+  auth: {
+    title: 'auth.title',
+    email: 'auth.email',
+    password: 'auth.password',
+    submit: 'auth.submit',
+    logout: 'auth.logout',
+  },
+  common: {
+    create: 'common.create',
+    edit: 'common.edit',
+    activate: 'common.activate',
+    deactivate: 'common.deactivate',
+    
+    save: 'common.save',
+    cancel: 'common.cancel',
+    back: 'common.back',
+    
+    select: 'common.select',
+    loading: 'common.loading',
+    requiredValue: 'common.requiredValue',
+    
+    showInactive: 'common.showInactive',
+    showActive: 'common.showActive',
+
+    title: 'common.title',
+    name: 'common.name',
+    phone: 'common.phone',
+    email: 'common.email',
+    password: 'common.password',
+    passwordRequired: 'common.passwordRequired',
+    role: 'common.role',
+    counterparty: 'common.counterparty',
+    hubs: 'common.hubs',
+
+    products: 'common.products',
+    counterparties: 'common.counterparties',
+  },
+  users: {
+    title: 'users.title',
+    roles: 'users.roles',
+  },
+  user: {
+    selectRole: 'user.selectRole',
+    selectCounterparty: 'user.selectCounterparty',
+    selectHubs: 'user.selectHubs',
+    enterName: 'user.enterName',
+    enterPhone: 'user.enterPhone',
+    enterEmail: 'user.enterEmail',
+    enterPassword: 'user.enterPassword',
+
+    roleRequired: 'user.roleRequired',
+    counterpartyRequired: 'user.counterpartyRequired',
+    hubsRequired: 'user.hubsRequired',
+    nameRequired: 'user.nameRequired',
+    phoneRequired: 'user.phoneRequired',
+    incorrectPhone: 'user.incorrectPhone',
+    emailRequired: 'user.emailRequired',
+    incorrectEmail: 'user.incorrectEmail',
+    passwordMinLength: 'user.passwordMinLength',
+    
+    editCurrent: 'user.editCurrent',
+    createNew: 'user.createNew',
+    
+    confirmDeactivate: 'user.confirmDeactivate',
+    updated: 'user.updated',
+    created: 'user.created',
+    activated: 'user.activated',
+    deactivated: 'user.deactivated'
+  },
+  product: {
+    name: 'product.name',
+
+    editCurrent: 'product.editCurrent',
+    createNew: 'product.createNew',
+    
+    confirmDeactivate: 'product.confirmDeactivate',
+    updated: 'product.updated',
+    created: 'product.created',
+    activated: 'product.activated',
+    deactivated: 'product.deactivated'
+  },
+  navigation: {
+    users: 'navigation.users',
+    roles: 'navigation.roles',
+    counterparties: 'navigation.counterparties',
+    hubs: 'navigation.hubs',
+    products: 'navigation.products',
+    orders: 'navigation.orders',
+    payments: 'navigation.payments',
+    certificates: 'navigation.certificates',
+  },
+  table: {
+    noData: 'table.noData',
+    filter: 'table.filter',
+    page: 'table.page',
+    of: 'table.of',
+    rows: 'table.rows',
+  }
+}
---
+++ src/core/i18n/locales/ru/index.js
@@ -0,0 +1,21 @@
+import auth from './auth.json';
+import common from './common.json';
+import counterparties from './counterparties.json';
+import navigation from './navigation.json';
+import product from './product.json';
+import products from './products.json';
+import table from './table.json';
+import user from './user.json';
+import users from './users.json';
+
+export default { 
+  auth,
+  common,
+  counterparties,
+  navigation,
+  product,
+  products,
+  table,
+  user,
+  users,
+};

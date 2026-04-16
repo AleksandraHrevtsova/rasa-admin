635f8f5332ef58388f941aed789af2cfa59c6962
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-04-17 02:04:19 +0300

    removed locale provider in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 // import { useState } from 'react'
 import { AuthProvider } from './contexts/AuthContext';
-import { LocaleProvider } from './contexts/LocaleContext';
 import AppRouter from './routes/AppRouter';
 import './core/api/interceptors';
 
 function App() {
 
   return (
-    <LocaleProvider>
       <AuthProvider>
         <AppRouter/>
       </AuthProvider>
-    </LocaleProvider>
   )
 }
 

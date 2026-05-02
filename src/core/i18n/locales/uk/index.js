016fe8919ac198a6ea74a257653bea536dbe3e1c
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-05-02 03:53:41 +0300

    added localizations

--- src/core/i18n/locales/uk/index.js
+++ src/core/i18n/locales/uk/index.js
@@ -1,21 +1,26 @@
 import auth from '@/core/i18n/locales/uk/auth.json';
-import common from '@/core/i18n/locales/uk/common.json';
-import counterparties from '@/core/i18n/locales/uk/counterparties.json';
 import navigation from '@/core/i18n/locales/uk/navigation.json';
-import product from '@/core/i18n/locales/uk/product.json';
-import products from '@/core/i18n/locales/uk/products.json';
+import common from '@/core/i18n/locales/uk/common.json';
 import table from '@/core/i18n/locales/uk/table.json';
-import user from '@/core/i18n/locales/uk/user.json';
+
 import users from '@/core/i18n/locales/uk/users.json';
+import user from '@/core/i18n/locales/uk/user.json';
+
+import products from '@/core/i18n/locales/uk/products.json';
+import product from '@/core/i18n/locales/uk/product.json';
+
+import counterparties from '@/core/i18n/locales/uk/counterparties.json';
+import counterparty from '@/core/i18n/locales/uk/counterparty.json';
 
 export default { 
   auth,
-  common,
-  counterparties,
   navigation,
-  product,
-  products,
+  common,
   table,
-  user,
   users,
+  user,
+  products,
+  product,
+  counterparties,
+  counterparty,
 };
